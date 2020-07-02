cf0a6c8636626f69602d2ebca43e6bc803e6bd17
ltvnkvtl <vital.litovk@gmail.com>
2020-07-02 09:08:53 +0300

    auth.service injections to Signup Login and Header. Added authChange(rxjs) in Auth service for dynamic edition of header navigation(try to login)

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
 
+import {AuthService} from '../auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,12 +10,15 @@ import {NgForm} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    this.authService.login({
+      email: form.value.email,
+      password: form.value.password
+    });
   }
 }
