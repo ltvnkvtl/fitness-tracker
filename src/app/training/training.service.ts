d6bb9c76136ce229a6dc13c447baa03b93091b86
ltvnkvtl <vital.litovk@gmail.com>
2020-07-02 14:59:42 +0300

    refactored new training start button with adding form that activate exerciseSubscription(rxjs) for activating Current training

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,14 +1,22 @@
 import {Exercise} from './exercise.model';
+import {Subject} from 'rxjs';
 
 export class TrainingService {
+  exterciseChanged = new Subject<Exercise>();
   private avaliableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
     { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
   ];
+  private runningExercise: Exercise;
 
   getAvaliableExercises() {
     return this.avaliableExercises.slice();
+  }
+
+  startExercise(selectedId: string) {
+    this.runningExercise = this.avaliableExercises.find(ex => ex.id === selectedId);
+    this.exterciseChanged.next({...this.runningExercise});
   }
 }
