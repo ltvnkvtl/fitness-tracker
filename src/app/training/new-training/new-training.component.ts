01b298e95b893b09b0d3705d5cd4dab092432eb0
ltvnkvtl <vital.litovk@gmail.com>
2020-07-03 06:42:12 +0300

    Added angularfirestore in project and connected New training mat-select with avaliableExercises(from server)

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {TrainingService} from '../training.service';
 import {Exercise} from '../exercise.model';
 import {NgForm} from '@angular/forms';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
@@ -9,12 +11,15 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./new-training.component.scss']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Exercise[] = [];
+  exercises: Observable<any>;
 
-  constructor(private trainingService: TrainingService) { }
+  constructor(
+    private trainingService: TrainingService,
+    private db: AngularFirestore
+  ) { }
 
   ngOnInit(): void {
-    this.exercises = this.trainingService.getAvaliableExercises();
+    this.exercises = this.db.collection('avaliableExercises').valueChanges();
   }
 
   onStartTraining(form: NgForm) {
