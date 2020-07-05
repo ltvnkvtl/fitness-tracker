9a2e788f31f0779e92af88b9b8d97897bb4bd5c5
ltvnkvtl <vital.litovk@gmail.com>
2020-07-05 13:11:21 +0300

    Edit: Auth guard + enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAfyOewNf8uuCp_H8lxaPRf4_OP3ygbL60',
+    authDomain: 'ng-fitness-tracker-c685e.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-c685e.firebaseio.com',
+    projectId: 'ng-fitness-tracker-c685e',
+    storageBucket: 'ng-fitness-tracker-c685e.appspot.com',
+    messagingSenderId: '542395032821',
+    appId: '1:542395032821:web:5f07f95f60ece638a6d1bc'
+  }
 };
