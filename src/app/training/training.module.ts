88ccb76039798c79d02702c8ee5ce3714121410e
ltvnkvtl <vital.litovk@gmail.com>
2020-07-05 13:03:14 +0300

    Refactoring routes. Added training-routing and auth routing modules. + lazy loading

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,16 +1,12 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {MaterialModule} from '../material.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import {SharedModule} from '../shared/shared.module';
+import {TrainingRoutingModule} from './training-routing.module';
 
 import {TrainingComponent} from './training.component';
 import {CurrentTrainingComponent} from './current-training/current-training.component';
 import {NewTrainingComponent} from './new-training/new-training.component';
 import {PastTrainingsComponent} from './past-training/past-trainings.component';
 import {StopTrainingComponent} from './current-training/stop-training.component';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -21,8 +17,8 @@ import {SharedModule} from '../shared/shared.module';
     StopTrainingComponent
   ],
   imports: [
-    AngularFirestoreModule,
-    SharedModule
+    SharedModule,
+    TrainingRoutingModule
   ],
   entryComponents: [StopTrainingComponent]
 })
